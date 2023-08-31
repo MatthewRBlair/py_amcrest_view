c2e2697cde8860640f0225486ba94c8edabcd5e1
MatthewRBlair <sevenstringblues@gmail.com>
2023-08-30 19:13:32 -0500

    us np delete

--- amcrest_multi_view.py
+++ amcrest_multi_view.py
@@ -108,7 +108,7 @@ async def main(args):
                     i = 0
                     for (x, y, w, h) in boxes:
                         if [x, y, w, h] in permanent_rectangles:
-                            weights.remove([x, y, w, h])
+                            np.delete(weights, [x, y, w, h])
                             continue
                         cv2.rectangle(stitched_frame, (x, y), (x + w, y + h), (0, 0, 255), 2)  # Draw red rectangles
                         if str([x, y, w, h]) not in rectangle_history:
